949e4b36bf1f46a337fca50021784c6c08ae732b
Hussein Al-Malki <saudihackron@gmail.com>
2026-07-22 05:57:54 +0300

    style(neo-tree): switch color palette from Everforest to Doom One

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -1,7 +1,8 @@
 -- Bootstrap Lazy
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable',
+    lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -14,6 +15,6 @@ require('lazy').setup('user.plugins', {
     notify = false,
   },
   install = {
-    colorscheme = { 'catppuccin' },
+    colorscheme = { 'doom-one' },
   },
 })
